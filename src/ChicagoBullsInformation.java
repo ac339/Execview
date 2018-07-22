85d2e973b586ec083c99893218d7e7a8de0e266a
ac339 <clearyalex92@gmail.com>
2018-07-22 14:10:27 +0100

    ChicagoBullsInformation
    Create class for ChicagoBullsInformation

---
+++ src/ChicagoBullsInformation.java
@@ -0,0 +1,100 @@
+public class ChicagoBullsInformation {
+
+    public String position;
+    public int number;
+    public String country;
+    public String name;
+    public  String height;
+    public String weight;
+    public String university;
+    public double PPG;
+
+    public ChicagoBullsInformation(String p, int n, String c, String na,String h, String w, String u, double ppg){
+        this.position=p;
+        this.number=n;
+        this.country=c;
+        this.name=na;
+        this.height=h;
+        this.weight=w;
+        this.university=u;
+        this.PPG=ppg;
+
+    }
+    public int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getHeight() {
+        return height;
+    }
+
+    public void setHeight(String height) {
+        this.height = height;
+    }
+
+    public String getWeight() {
+        return weight;
+    }
+
+    public void setWeight(String weight) {
+        this.weight = weight;
+    }
+
+    public String getUniversity() {
+        return university;
+    }
+
+    public void setUniversity(String university) {
+        this.university = university;
+    }
+
+    public double getPPG() {
+        return PPG;
+    }
+
+    public void setPPG(double PPG) {
+        this.PPG = PPG;
+    }
+
+
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,45 @@
+import java.io.*;
+
 public class Main {
+    BufferedReader csvFile = null;
+    BufferedWriter jsonFile = null;
+    public static void main(String[] args) {
+
+        try {
+        BufferedReader csvFile= new BufferedReader(new FileReader("src/chicago-bulls.csv"));
+        BufferedWriter jsonFile=new BufferedWriter(new FileWriter("src/chicago-bulls.json"));
+
+        String fileContent = csvFile.readLine();
+
+// set the constant header
+        String jsonScript="constant header of json content";
+
+        while (fileContent !=null) {
+            fileContent = csvFile.readLine();
+            String[] tab = fileContent.split(",");
+            // variable content from csv file
+            jsonScript += "\"" + tab[0] + "\" :";
+            jsonScript += tab[1] + "," + "\n";
+
+            // End of json content construction
+            jsonScript = jsonScript.substring(0, jsonScript.length() - 2);
+            jsonScript += "}";
+
+            String[] tabWrite = jsonScript.split("\n");
+            for (String item : tabWrite) {
+                jsonFile.write(item);
+                jsonFile.newLine();
+            }
+
+        }
+            csvFile.close();
+            jsonFile.close();
+    } catch (FileNotFoundException ex) {
+      System.out.println(ex);
+    } catch (IOException ex) {
+            System.out.println(ex);
+    }
+
 }
+}
+
